d8078c5f0c43c1f33b965c49b252414160acec42
darlingmota <darlingmotta@gmail.com>
2026-05-01 17:49:33 +0100

    resize once hit 75% full

--- hash_map.py
+++ hash_map.py
@@ -3,3 +3,12 @@ class HashMapNode:
         self.key = key
         self.value = value
         self.next = None
+
+
+class HashMap:
+    LOAD_FACTOR_THRESHOLD = 0.75
+
+    def __init__(self, initial_capacity: int = 1024):
+        self._capacity = initial_capacity
+        self._size = 0
+        self._buckets = [None] * self._capacity
