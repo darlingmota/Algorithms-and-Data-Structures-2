439331f19ee68785e9fe39c82bd4b0d02640023f
darlingmota <darlingmotta@gmail.com>
2026-05-01 17:18:02 +0100

    trials time and returning avg time

--- benchmark.py
+++ benchmark.py
@@ -15,3 +15,14 @@ DATASET_SIZES = [500, 1000, 2000, 5000, 10000, 20000, 27278]
 PREFIX_QUERIES = ["the", "star", "da", "a", "love", "man", "night", "dark", "war", "inc"]
 
 EXACT_QUERY_COUNT = 20
+
+
+
+def _average_time(func, trials: int = TRIALS) -> float:
+    times = []
+    for _ in range(trials):
+        start = time.perf_counter()
+        func()
+        end = time.perf_counter()
+        times.append(end - start)
+    return sum(times) / len(times)
