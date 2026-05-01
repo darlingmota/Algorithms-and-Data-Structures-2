53d0eca9cdb8c70c920e3510670fe56179fff458
darlingmota <darlingmotta@gmail.com>
2026-05-01 17:50:51 +0100

    checck if we need to resize before addin

--- hash_map.py
+++ hash_map.py
@@ -1,3 +1,5 @@
+
+
 class HashMapNode:
     def __init__(self, key, value):
         self.key = key
@@ -6,12 +8,14 @@ class HashMapNode:
 
 
 class HashMap:
+
     LOAD_FACTOR_THRESHOLD = 0.75
 
     def __init__(self, initial_capacity: int = 1024):
         self._capacity = initial_capacity
         self._size = 0
         self._buckets = [None] * self._capacity
+
     def _hash(self, key) -> int:
         key_str = str(key).lower()
         hash_value = 0
@@ -20,3 +24,7 @@ class HashMap:
         for char in key_str:
             hash_value = (hash_value * prime + ord(char)) % mod
         return hash_value
+
+    def insert(self, key, value):
+        if (self._size / self._capacity) >= self.LOAD_FACTOR_THRESHOLD:
+            self._resize()
