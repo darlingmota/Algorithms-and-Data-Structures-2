f82cc4a4f3660ec120749392bd87ad49bc2aec36
darlingmota <darlingmotta@gmail.com>
2026-05-01 18:00:55 +0100

    0(k) len of title

--- trie.py
+++ trie.py
@@ -9,3 +9,15 @@ class Trie:
 
     def __init__(self):
         self._root = TrieNode()
+    def insert(self, title: str, movie):
+        key = title.lower()
+        node = self._root
+
+        for char in key:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+
+        node.is_end_of_word = True
+        node.movies.append(movie)
+        self._size += 1
