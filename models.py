f423fe9cfd23caf6964104f9e52d2fd801a8ff47
darlingmota <darlingmotta@gmail.com>
2026-05-01 17:59:03 +0100

    split into list

--- models.py
+++ models.py
@@ -4,3 +4,6 @@ class Movie:
                  avg_rating: float = 0.0, rating_count: int = 0):
         self.movie_id = movie_id
         self.title = title
+        self.genres = [g.strip() for g in genres.split("|")] if genres else []
+        self.avg_rating = avg_rating
+        self.rating_count = rating_count
