bcc587ceefa4848a733446cc56e62c6a7a73aae1
darlingmota <darlingmotta@gmail.com>
2026-05-02 07:14:20 +0100

    libraries

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+import os
+import random
+
+from data_loader import load_movies
+from hash_map import HashMap
+from trie import Trie
+from benchmark import (
+    benchmark_insertion,
+    benchmark_exact_search,
+    benchmark_prefix_search,
+    benchmark_trie_vs_hashmap_exact,
+    benchmark_memory_usage,
+    print_summary,
+    DATASET_SIZES,
+)
+
